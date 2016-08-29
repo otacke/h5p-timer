6b718937496f025b624d5967b3acd3c955bf1b30
otacke <o.tacke@web.de>
2016-08-29 22:28:40 +0200

    prepare for countdown feature

--- scripts/timer.js
+++ scripts/timer.js
@@ -5,6 +5,7 @@ var H5P = H5P || {};
  *
  * TODO: something like "notifyAfterPlaying(milliSeconds, callback, params)"
  * TODO: something like "notifyEvery(milliSeconds, callback, params)"
+ * TODO: countdown feature
  *
  * @param {H5P.jQuery} $
  */
@@ -13,17 +14,17 @@ H5P.Timer = (function ($) {
    * Create a timer.
    * @constructor
    *
-   * @param interval - The update interval.
+   * @param {Number} interval - The update interval.
    */
   function Timer(interval) {
     var self = this;
 
     if (!interval) {
-      interval = 100;
+      interval = Timer.DEFAULT_INTERVAL;
     }
 
     var status = Timer.STOPPED;
-    
+
     var cacheMilliSeconds = 0;
     var playingMilliSeconds = 0;
 
@@ -59,6 +60,16 @@ H5P.Timer = (function ($) {
         return (new Date().getTime() - startDate);
       } else {
         return lastDate.getTime() - firstDate;
+      }
+    }
+
+    /**
+     * Set the starting time.
+     *
+     * @param {Number} time - The time in milliSeconds or timecode.
+     */
+    this.setTime = function(time) {
+      if (status === Timer.STOPPED) {
       }
     }
 
@@ -99,22 +110,22 @@ H5P.Timer = (function ($) {
       cacheMilliSeconds += update(Timer.PAUSED);
       playingMilliSeconds = cacheMilliSeconds;
       startDate = undefined;
-      status = Timer.PAUSED;      
+      status = Timer.PAUSED;
     }
 
     /**
      * Stop the timer.
-     */    
+     */
     this.stop = function() {
       if (status !== Timer.PLAYING) {
         return;
-      }        
-      
+      }
+
       playingMilliSeconds = cacheMilliSeconds + update(Timer.STOPPED);
       lastDate = new Date();
-      status = Timer.STOPPED;      
+      status = Timer.STOPPED;
     }
-    
+
    /**
      * Update the timer until final call.
      *
@@ -127,13 +138,13 @@ H5P.Timer = (function ($) {
       if (targetStatus === Timer.PLAYING) {
         loop = setTimeout(function () {
           update(Timer.PLAYING);
-        }, interval);        
+        }, interval);
       } else {
         clearTimeout(loop);
         return currentMilliSeconds;
       }
     }
-    
+
     /**
      * Humanize time down to tenth of seconds.
      *
@@ -142,7 +153,7 @@ H5P.Timer = (function ($) {
      */
     var humanize = function(milliSeconds) {
     }
-    
+
     /**
      * Dehumanize time from down to tenth of seconds.
      *
@@ -151,18 +162,8 @@ H5P.Timer = (function ($) {
      */
     var humanize = function(timecode) {
     }
-    
-    /**
-     * Check if variable of type Number.
-     *
-     * @param {Object} test - The test object.
-     * @return {boolean} True or False.
-     */
-    var isNumber = function(test) {
-      return !isNaN(parseFloat(test))
-    }
   }
-  
+
   // Timer states
   /** @constant {Number} */
   Timer.STOPPED = 0;
@@ -170,12 +171,15 @@ H5P.Timer = (function ($) {
   Timer.PLAYING = 1;
   /** @constant {Number} */
   Timer.PAUSED = 2;
-  
+
   // Timer direction
   /** @constant {Number} */
   Timer.FORWARD = 1;
   /** @constant {Number} */
   Timer.BACKWARD = -1;
-  
-  return Timer;  
+
+  /** @constant {Number} */
+  Timer.DEFAULT_INTERVAL = 10;
+
+  return Timer;
 })(H5P.jQuery);
